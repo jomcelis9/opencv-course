97747a8154e0fd818f1a4c98e22b375135c0a803
Jomari Quiñonez Celis <jomaricelis@gmail.com>
2025-08-19 07:36:00 +0800

    rara

--- Section #1 - Basics/basic_functions.py
+++ Section #1 - Basics/basic_functions.py
@@ -2,34 +2,68 @@
 
 import cv2 as cv
 
-# Read in an image
-img = cv.imread('../Resources/Photos/park.jpg')
-cv.imshow('Park', img)
+# # READ IMAGES
+# img = cv.imread('Resources/Photos/cat_large.jpg')
+# cv.imshow('Cat',img)
+# cv.waitKey(0)
 
-# Converting to grayscale
-gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
-cv.imshow('Gray', gray)
 
-# Blur 
-blur = cv.GaussianBlur(img, (7,7), cv.BORDER_DEFAULT)
-cv.imshow('Blur', blur)
+# RESIZE VIDEOS
+def rescaleFrame(frame, scale = 0.75):
+    
+    width = int(frame.shape[1]*scale)
+    height = int(frame.shape[0]*scale)
 
-# Edge Cascade
-canny = cv.Canny(blur, 125, 175)
-cv.imshow('Canny Edges', canny)
+    dimensions = (width,height)
 
-# Dilating the image
-dilated = cv.dilate(canny, (7,7), iterations=3)
-cv.imshow('Dilated', dilated)
+    return cv.resize(frame, dimensions, interpolation=cv.INTER_AREA)
 
-# Eroding
-eroded = cv.erode(dilated, (7,7), iterations=3)
-cv.imshow('Eroded', eroded)
+# READ VIDEOS
 
-# Resize
-resized = cv.resize(img, (500,500), interpolation=cv.INTER_CUBIC)
-cv.imshow('Resized', resized)
+capture = cv.VideoCapture('Resources/Videos/dog.mp4')
 
-# Cropping
-cropped = img[50:200, 200:400]
-cv.imshow('Cropped', cropped)
+while True:
+    isTrue, frame = capture.read()
+    frame_resized = rescaleFrame(frame)
+    cv.imshow('Video', frame_resized)
+
+    if cv.waitKey(20) & 0xFF==ord('d'):
+        break
+
+capture.release()
+cv.destroyAllWindows()
+
+
+
+
+# # Read in an image
+# img = cv.imread('../Resources/Photos/park.jpg')
+# cv.imshow('Park', img)
+
+# # Converting to grayscale
+# gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
+# cv.imshow('Gray', gray)
+
+# # Blur 
+# blur = cv.GaussianBlur(img, (7,7), cv.BORDER_DEFAULT)
+# cv.imshow('Blur', blur)
+
+# # Edge Cascade
+# canny = cv.Canny(blur, 125, 175)
+# cv.imshow('Canny Edges', canny)
+
+# # Dilating the image
+# dilated = cv.dilate(canny, (7,7), iterations=3)
+# cv.imshow('Dilated', dilated)
+
+# # Eroding
+# eroded = cv.erode(dilated, (7,7), iterations=3)
+# cv.imshow('Eroded', eroded)
+
+# # Resize
+# resized = cv.resize(img, (500,500), interpolation=cv.INTER_CUBIC)
+# cv.imshow('Resized', resized)
+
+# # Cropping
+# cropped = img[50:200, 200:400]
+# cv.imshow('Cropped', cropped)
